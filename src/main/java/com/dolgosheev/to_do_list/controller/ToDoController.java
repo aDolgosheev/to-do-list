bebb6e671fd00b4b4b8ae94e2734b54889e2931b
Aleksandr Dolgosheev <al.dolgosheev@gmail.com>
2024-11-19 15:18:58 +0300

    2024.11.19 update

--- src/main/java/com/dolgosheev/to_do_list/controller/ToDoController.java
+++ src/main/java/com/dolgosheev/to_do_list/controller/ToDoController.java
@@ -2,12 +2,21 @@ package com.dolgosheev.to_do_list.controller;
 
 import com.dolgosheev.to_do_list.model.ToDoItem;
 import com.dolgosheev.to_do_list.repository.ToDoItemRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 @Controller
-public class ToDoController {
+public class ToDoController implements CommandLineRunner {
 
     private final ToDoItemRepository toDoItemRepository;
 
@@ -17,13 +26,55 @@ public class ToDoController {
 
     @GetMapping
     public String index(Model model) {
-        toDoItemRepository.save(new ToDoItem("Item 1"));
-        toDoItemRepository.save(new ToDoItem("Item 2"));
+        List<ToDoItem> toDoItemRepositoryAll = toDoItemRepository.findAll();
+        model.addAttribute("toDoItemRepositoryAll", toDoItemRepositoryAll);
+        model.addAttribute("newToDo", new ToDoItem());
 
-        model.addAttribute("data", "Hello");
         return "index";
     }
 
+    @PostMapping("/add")
+    public String add(@ModelAttribute ToDoItem toDoItem) {
+        toDoItemRepository.save(toDoItem);
 
+        return "redirect:/";
+    }
 
+    @PostMapping("/delete/{id}")
+    public String deleteToDoItem(@PathVariable("id") Long id) {
+        toDoItemRepository.deleteById(id);
+
+        return "redirect:/";
+    }
+
+    @PostMapping("/removeAll")
+    public String removeAllItems() {
+        toDoItemRepository.deleteAll();
+
+        return "redirect:/";
+    }
+
+    @PostMapping("/search")
+    public String searchToDoItems(@RequestParam("searchTerm") String searchTerm, Model model) {
+        List<ToDoItem> allItems = toDoItemRepository.findAll();
+        List<ToDoItem> searchResults = new ArrayList<>();
+
+        for (ToDoItem item : allItems) {
+            if (item.getTitle().toLowerCase().contains(searchTerm.toLowerCase(Locale.ROOT))) {
+                searchResults.add(item);
+            }
+        }
+
+        model.addAttribute("toDoItemRepositoryAll", searchResults);
+        model.addAttribute("newToDo", new ToDoItem());
+        model.addAttribute("searchTerm", searchTerm);
+
+        return "index";
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        toDoItemRepository.save(new ToDoItem("Item 1"));
+        toDoItemRepository.save(new ToDoItem("Item 2"));
+    }
 }
